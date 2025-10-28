ce7c9a4bd48ec11ba408dcd536ed763197a12aad
SzymonMilcz <szymonmil04@gmail.com>
2025-10-28 19:52:54 +0100

    Player detection script finished
    Also managed to make the enemy spawn projectiles to attack the player, though it needs more work to work properly

--- Assets/Scripts/ShootingEnemyBehaviour.cs
+++ Assets/Scripts/ShootingEnemyBehaviour.cs
@@ -3,27 +3,57 @@ using UnityEngine;
 
 public class ShootingEnemyBehaviour : MonoBehaviour
 {
-    bool PlayerDetected = false;
-    float detectionSize = 10F;
+    public bool PlayerDetected = false;
+    float detectionSize = 100F;
     Vector2 detectionOriginOffset = Vector2.zero;
     Vector2 detectionOrigin;
-    public LayerMask PlayerLayer;
+    public LayerMask playerLayer;
+    RaycastHit2D detectedObject;
+    float detectionRefreshTimer = 3;
+    public Rigidbody2D projectile;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        detectionRefreshTimer += Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (detectionRefreshTimer < Time.time)
+        {
+            PlayerDetectionCheck();
+        }
     }
 
     void PlayerDetectionCheck()
     {
         detectionOrigin = new Vector2(transform.localPosition.x, transform.localPosition.y);
-        Debug.Log(Physics2D.CircleCast(detectionOrigin, detectionSize, detectionOriginOffset, 0F, PlayerLayer));
+        detectedObject = Physics2D.CircleCast(detectionOrigin, detectionSize, detectionOriginOffset, 0F, playerLayer);
+        if (detectedObject)
+        {
+            if (PlayerDetected == false)
+            {
+                PlayerDetected = true;
+                detectionRefreshTimer += 3;
+            }
+            else
+            {
+                ShootProjectile();
+                detectionRefreshTimer += 3;
+            }            
+            Debug.Log("Player found");
+        }
+        else
+        {
+            PlayerDetected = false;
+
+        }
+    }
+
+    void ShootProjectile()
+    {
+        Instantiate(projectile, transform, transform);
     }
 }
