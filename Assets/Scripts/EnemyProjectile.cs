44b6ac739f3970dafa492db5903c3df17cd1dfa3
SzymonMilcz <szymonmil04@gmail.com>
2025-10-30 17:51:59 +0100

    Enemy projectile behavior finished

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
-    public int damageValue; 
+    public int damageValue;
+    public Collider2D projectileCollider;
+    public LayerMask projectileLayer;
+    public float projectileLifetime;
     void Start()
     {
-        
+        Physics2D.IgnoreLayerCollision(6, 6);
+        projectileLifetime += Time.time;
     }
 
     void Update()
     {
-
+        if (projectileLifetime < Time.time)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D touchedObj)
